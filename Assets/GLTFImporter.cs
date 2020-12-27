20fc2697d4f53e4aa962cc85dfa583e114a80303
Trevor <trevorj342@gmail.com>
2020-12-27 17:32:58 -0500

    Import UniVRM & Create Basic GLTF Importer

---
+++ Assets/GLTFImporter.cs
@@ -0,0 +1,19 @@
+﻿using UniGLTF;
+using UnityEngine;
+
+public class GLTFImporter : MonoBehaviour
+{
+    public string pathToGltf;
+
+    public void Start()
+    {
+        LoadGLTF();
+    }
+
+    public void LoadGLTF()
+    {
+        var context = new ImporterContext();
+        context.Load(pathToGltf);
+        context.ShowMeshes();
+    }
+}
